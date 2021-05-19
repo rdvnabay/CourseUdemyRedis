4675dd69a3d45ff4e2b525c423b3bbc9aaef7d3b
Rıdvan <ridvanabay@gmail.com>
2021-05-19 22:58:43 +0300

    4.Bölüm 27.Ders - IDistributedCache-2 (Complex Type’ların cachlenmesi)

--- IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
@@ -15,15 +18,33 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         public async Task<IActionResult> Index()
         {
             DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
-            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _distributedCache.SetString("name", "Rıdvan",options);
-            await _distributedCache.SetStringAsync("surName", "Abay");
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(2);
+
+            //Json
+            Product product = new Product { Id = 1, Name = "Phone", Price = 4000 };
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            await _distributedCache.SetStringAsync("product:1", jsonProduct,options);
+
+            //Binary
+            Product product2 = new Product { Id = 2, Name = "PC", Price = 6000 };
+            string jsonProduct2 = JsonConvert.SerializeObject(product2);
+            Byte[] binaryProduct = Encoding.UTF8.GetBytes(jsonProduct2);
+            _distributedCache.Set("product:2",binaryProduct,options);
             return View();
         }
 
         public IActionResult Show() {
-            string name=_distributedCache.GetString("name");
-            ViewBag.Name = name;
+            //Json
+            string jsonProduct = _distributedCache.GetString("product:1");
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            //Binary
+            Byte[] binaryProduct = _distributedCache.Get("product:2");
+            string stringProduct = Encoding.UTF8.GetString(binaryProduct);
+            Product product2 = JsonConvert.DeserializeObject<Product>(stringProduct);
+
+            ViewBag.Product = product;
+            ViewBag.Product2 = product2;
             return View();
         }
 
