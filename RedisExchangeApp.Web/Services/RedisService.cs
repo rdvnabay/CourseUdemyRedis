ecef1050af5aed00df38f8b51e2aa4af0cc62fd4
Rıdvan <ridvanabay@gmail.com>
2021-05-20 00:15:28 +0300

    5.Bölüm 29.Ders - StackExchange.Redis API

---
+++ RedisExchangeApp.Web/Services/RedisService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeApp.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private IDatabase database;
+        private ConnectionMultiplexer _redis;
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var config = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(config);
+ 
+        }
+
+        public IDatabase GetDb(int db)
+        {
+           return _redis.GetDatabase(db);
+        }
+    }
+}
